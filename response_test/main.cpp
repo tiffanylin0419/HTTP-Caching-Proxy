731dba83f69760e566ae5562b4dd48c0a9e06139
Tiffany <tl330@duke.edu>
2023-02-21 00:27:55 -0500

    getRequest done

---
+++ response_test/main.cpp
@@ -0,0 +1,26 @@
+
+#include"GetRequest.h"
+
+
+
+int main() {  
+
+  std::string str="GET http://detectportal.firefox.com/canonical.html HTTP/1.1\n"
+"Host: detectportal.firefox.com\n"
+"User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36\n"
+"Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9\n"
+"Accept-Encoding: gzip, deflate, br\n"
+"Accept-Language: en-US,en;q=0.9\n"
+"Connection: keep-alive\n"
+"Date: Thu, 24 Feb 2023 15:30:00 GMT\n"
+"Expires: Thu, 24 Feb 2023 16:30:00 GMT\n"
+"Last-Modified: Wed, 23 Feb 2023 12:00:00 GMT\n"
+"ETag: \"example-12345\"\n"
+"Cache-Control: max-age=3601\n";
+
+  GetRequest req=GetRequest(str);
+  req.print();
+  
+  return 0;
+}
+
---
+++ response_test/proxy_function.h
@@ -0,0 +1,26 @@
+#include "head.h"
+
+int get_number(std::string& s) {
+  int ans=0;
+  for (std::string::iterator it = s.begin(); it != s.end(); ++it) {
+    char c = *it;
+    if (!isdigit(c)) {
+      return -1;
+    }
+    ans=ans*10+static_cast<int>(c)-static_cast<int>('0');
+  }
+  return ans;
+}
+
+int getContextLength(const char * server_msg, int mes_len) {
+  std::string input(server_msg, mes_len);
+  std::stringstream ss(input);
+  std::vector<std::string> parsedSpace((std::istream_iterator<std::string>(ss)), std::istream_iterator<std::string>());
+  for(int i=0;i<parsedSpace.size();i++){
+    if(parsedSpace[i]=="Content-Length:"){
+      std::string hostLine=parsedSpace[i+1];
+      return get_number(hostLine);
+    }
+  }
+  return -1;
+}
